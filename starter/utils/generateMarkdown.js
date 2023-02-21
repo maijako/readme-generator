4da4621b6ccd21f45f5a42f8a48c7eddf13ca22a
ensignjava <109529105+maijako@users.noreply.github.com>
2023-02-21 13:55:57 +0000

    add template literal to generateMarkdown

--- starter/utils/generateMarkdown.js
+++ starter/utils/generateMarkdown.js
@@ -1,7 +1,39 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
+  
+  ## Description
+  ${description}
+  
+  ## Table of Contents
+  1. [Installation](#installation)
+  2. [Usage](#usage)
+  3. [License](#licence)
+  4. [Contributing](#contributing)
+  5. [Tests](#tests)
+  6. [Questions](#questions)
+  
+  ## Installation
+  ${installation}
+  
+  ## Usage
+  ${usage}
+  
+  
+  ## Licence
+  
+  This project uses ${license}.
+  
+  ## Contributing
+  ${contributing}
+  
+  
+  ## Tests
+  ${tests}
+  
+  ## Questions
+  This project was developed by ${[gitUserName]}${(gitUserLink)}.
+  Please email any questions to ${userEmail}.
 `;
 }
 
