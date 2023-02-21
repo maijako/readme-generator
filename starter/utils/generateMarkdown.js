1dae7017b55d947abe756c091ee102596f4fb6aa
ensignjava <109529105+maijako@users.noreply.github.com>
2023-02-21 14:31:32 +0000

    add template literals to generateMarkdown

--- starter/utils/generateMarkdown.js
+++ starter/utils/generateMarkdown.js
@@ -8,7 +8,7 @@ function generateMarkdown(data) {
   ## Table of Contents
   1. [Installation](#installation)
   2. [Usage](#usage)
-  3. [License](#licence)
+  3. [License](#license)
   4. [Contributing](#contributing)
   5. [Tests](#tests)
   6. [Questions](#questions)
