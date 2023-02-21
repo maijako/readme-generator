8855ae9e792543404bb7d600b0dd81cc7fe281fe
ensignjava <109529105+maijako@users.noreply.github.com>
2023-02-21 09:02:45 +0000

    list of licenses added

--- starter/index.js
+++ starter/index.js
@@ -5,7 +5,38 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-
+    {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'Please enter a short description of your project:',
+        name: 'description',
+    },
+    {
+        type: 'input', 
+        message: 'Please enter installation instructions for your project.',
+        name: 'installation'
+    },
+    {
+        type: 'input', 
+        message: 'Please enter installation instructions for your project.',
+        name: 'installation'
+    },
+    {
+        type: 'input',
+        message: 'Please enter usage recommendation for your project.',
+        name: 'usage'
+    },
+    {
+        type: 'list', 
+        message: 'Please select the license type for your project.',
+        name: 'license',
+        choices: ['Artistic license 2.0', 'Educational Community License v2.0', 
+        'MIT', 'Boost Software License 1.0', 'IBM Public License Version 1.0', 'ISC']
+    }
 ];
 
 // function to write README file
